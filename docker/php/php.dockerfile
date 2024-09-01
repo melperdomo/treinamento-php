6c383599934844f127487636178bef8ae535fb34
Lucian Alves <lucian.alves@proponto.com.br>
2024-09-01 15:54:13 -0300

    Configurações de fuso horário

--- docker/php/php.dockerfile
+++ docker/php/php.dockerfile
@@ -9,7 +9,7 @@ RUN apt install sudo nano zip git -y
 
 # Extensions
 ADD --chmod=0755 https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions /usr/local/bin/
-RUN install-php-extensions curl zip sqlite3 mbstring xml pgsql pdo_pgsql
+RUN install-php-extensions curl zip sqlite3 mbstring xml pgsql pdo_pgsql intl
 
 # Composer
 RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
---
+++ exercicios/01-echo/exercicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Exercícos com echo</h1>
+    <p>Use o comando echo para exibir nesta página: </p>
+
+    <ul>
+        <li>Seu nome: </li>
+        <li>Sua idade daqui a 5 anos atrás (calcule com o PHP): </li>
+        <li>Data e hora atual (sem os segundos): </li>
+        <li>Qual dia do mês será daqui a 7 dias (data + cálculo): </li>
+    </ul>
+
+</body>
+</html>
