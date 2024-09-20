ac4ea8924b2ce3782f2730325bb64504d4ce8f12
Melissa Perdomo <melissameira92@gmail.com>
2024-09-20 17:01:05 -0300

    Exercícios com for.

---
+++ exercicios/09-for/soma-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Simples</title>
+</head>
+<body>
+    <h3>Animais Belíssimos</h3>
+    <?php $animais = ['Gatos', 'Tigres', 'Pumas', 'Racoons', 'Raposas'];?>
+    <?php echo $animais[0];?></br>
+    <?php echo $animais[2];?></br>
+    <?php echo $animais[4];?></br>
+</body>
+</html>
